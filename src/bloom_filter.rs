db4cb921a56b7c22feba6475ad398521af25fcd0
colmpat <cplang@dons.usfca.edu>
2023-05-16 22:25:00 -0700

    feat: Add hashing and get/put

--- src/bloom_filter.rs
+++ src/bloom_filter.rs
@@ -1,54 +1,52 @@
-pub struct BloomFilter {
-    bits: BitVec,
-    k: usize,
-}
+use std::hash::{Hash, Hasher};
+use seahash::SeaHasher;
+use rand::Rng;
 
-struct BitVec {
-    bits: Vec<u8>,
+pub struct BloomFilter {
+    bits: Vec<bool>,
+    hash_functions: Vec<SeaHasher>,
 }
 
 impl BloomFilter {
-    pub fn new(k: usize, count: usize) -> BloomFilter {
+    pub fn new(m: usize, k: usize) -> BloomFilter {
+        let mut rng = rand::thread_rng();
         BloomFilter {
-            bits: BitVec::new(),
-            k,
-        }
-    }
-}
-
-impl BitVec {
-    pub fn new() -> BitVec {
-        BitVec { bits: vec![] }
-    }
-
-    pub fn set(&mut self, i: usize, bit: bool) {
-        let byte_index = i >> 3;
-        let bit_index = i & 0b111;
-
-        // resize the vector if necessary
-        if self.bits.len() <= byte_index {
-            self.bits.resize(byte_index + 1, 0);
-        }
-
-        // set the bit
-        if bit {
-            self.bits[byte_index] |= 1 << bit_index;
-        } else {
-            self.bits[byte_index] &= !(1 << bit_index);
+            bits: vec![false; m],
+            hash_functions: (0..k)
+                .map(|_| {
+                    let seed:Vec<u64> = (0..4).map(|_| rng.gen()).collect();
+                    SeaHasher::with_seeds(seed[0], seed[1], seed[2], seed[3])
+                })
+                .collect(),
         }
     }
 
-    pub fn get(&self, i: usize) -> bool {
-        let byte_index = i >> 3;
-        let bit_index = i & 0b111;
-
-        // return false if the vector is too small
-        if self.bits.len() <= byte_index {
-            return false;
+    // adds the string to the set
+    pub fn put<T: Hash>(&mut self, val: T) {
+        for hash in self.hash(val) {
+            self.bits[hash] = true;
         }
+    }
 
-        // return the bit
-        let bit_identifier = 1 << bit_index;
-        return (self.bits[byte_index] & bit_identifier) != 0
+    // returns false if s is definitely not in the set and true if s might be in the set
+    pub fn get<T: Hash>(&self, val: T) -> bool {
+        for hash in self.hash(val) {
+            if !self.bits[hash] {
+                return false;
+            }
+        }
+        true
+    }
+
+    // returns a vector of indexes into the bit vector for the given string
+    fn hash<T: Hash>(&self, val: T) -> Vec<usize> {
+        let mut hashes = Vec::new();
+        for hash_function in &self.hash_functions {
+            let mut hasher = hash_function.clone();
+            val.hash(&mut hasher);
+            let hash = hasher.finish() as usize;
+            hashes.push(hash % self.bits.capacity());
+        }
+        hashes
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+mod bloom_filter;
+use std::{hash::Hash, fmt::Debug};
+
+use bloom_filter::BloomFilter;
+
+impl BloomFilter {
+    fn put_print<T: Hash + Debug>(&mut self, val: T) {
+        println!("putting {:?}", val);
+        self.put(val);
+    }
+
+    fn get_print<T: Hash + Debug>(&mut self, val: T) {
+        println!("getting {:?}", val);
+        println!("{}", self.get(val));
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let m = 10 * 1024 * 1024;
+    let k = 10;
+
+    let mut bf = BloomFilter::new(m, k);
+
+    bf.put_print("hello");
+    bf.get_print("hello");
+
+    bf.put_print("world");
+    bf.get_print("world");
+
+    bf.get_print("foo");
+    bf.get_print("bar");
+
+    bf.put_print("foo");
+    bf.put_print("bar");
+    bf.put_print("baz");
+
+    bf.get_print("bar");
 }
