bd8a04c0cf92184adaa676add017195064e303fc
colmpat <cplang@dons.usfca.edu>
2023-05-17 17:10:28 -0700

    fix: BloomFilter is now generic over some Hashable T

--- src/bloom_filter.rs
+++ src/bloom_filter.rs
@@ -1,14 +1,15 @@
-use std::hash::{Hash, Hasher};
+use std::{hash::{Hash, Hasher}, marker::PhantomData};
 use seahash::SeaHasher;
 use rand::Rng;
 
-pub struct BloomFilter {
+pub struct BloomFilter<T> {
     bits: Vec<bool>,
     hash_functions: Vec<SeaHasher>,
+    pd: PhantomData<T>
 }
 
-impl BloomFilter {
-    pub fn new(m: usize, k: usize) -> BloomFilter {
+impl<T: Hash> BloomFilter<T> {
+    pub fn new(m: usize, k: usize) -> BloomFilter<T> {
         let mut rng = rand::thread_rng();
         BloomFilter {
             bits: vec![false; m],
@@ -18,18 +19,19 @@ impl BloomFilter {
                     SeaHasher::with_seeds(seed[0], seed[1], seed[2], seed[3])
                 })
                 .collect(),
+            pd: PhantomData
         }
     }
 
     // add an elem of type T to the set
-    pub fn put<T: Hash>(&mut self, val: &T) {
+    pub fn put(&mut self, val: &T) {
         for hash in self.hash(val) {
             self.bits[hash] = true;
         }
     }
 
     // check if an elem of type T is in the set
-    pub fn get<T: Hash>(&self, val: &T) -> bool {
+    pub fn get(&self, val: &T) -> bool {
         for hash in self.hash(val) {
             if !self.bits[hash] {
                 return false;
@@ -39,7 +41,7 @@ impl BloomFilter {
     }
 
     // hash an elem of type T to a list of indices
-    fn hash<T: Hash>(&self, val: T) -> Vec<usize> {
+    fn hash(&self, val: &T) -> Vec<usize> {
         let mut hashes = Vec::new();
         for hash_function in &self.hash_functions {
             let mut hasher = hash_function.clone();
--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,17 @@
 mod bloom_filter;
-use std::{hash::Hash, fmt::Debug};
 
 use bloom_filter::BloomFilter;
-
-impl BloomFilter {
-    fn put_print<T: Hash + Debug>(&mut self, val: T) {
-        println!("putting {:?}", val);
-        self.put(&val);
-    }
-
-    fn get_print<T: Hash + Debug>(&mut self, val: T) {
-        println!("getting {:?}: {}", val, self.get(&val));
-    }
-}
 
 fn main() {
     let m = 10 * 1024 * 1024;
     let k = 10;
 
     let mut bf = BloomFilter::new(m, k);
-    let test = "hello";
-    bf.put_print(test);
-    bf.get_print(test);
 
-    bf.put_print("hello");
-    bf.get_print("hello");
+    // add a string to the set
+    println!("Adding 'hello' to the set");
+    bf.put(&"hello");
 
-    bf.put_print("world");
-    bf.get_print("world");
-
-    bf.get_print("foo");
-    bf.get_print("bar");
-
-    bf.put_print("foo");
-    bf.put_print("bar");
-    bf.put_print("baz");
-
-    bf.get_print("bar");
+    // check if a string is in the set
+    println!("'hello' is in the set: {}", bf.get(&"hello"));
 }
